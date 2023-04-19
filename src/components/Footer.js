6de7bf8125820b0f3bc5c4e178342ce0e9cdf5e3
Ismael Freitas <ismael.freitas@sapo.pt>
2023-04-19 13:32:08 +0100

    Fix overflow issue in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,13 +16,13 @@ function Footer() {
             boxShadow: "0px -1px 9px 0px rgba(77,77,77,1)"
         }}>
             <Box my={0.5} mx={2}>
-                <Typography variant="h6">Cristian Palao</Typography>
+                <Typography variant="h6" sx={{fontSize: "1rem"}}>Cristian Palao</Typography>
                 <a href="https://www.linkedin.com/in/cristian-pc/" rel="noreferrer" target="_blank"><LinkedIn fontSize="medium"/></a>
                 <a href="https://github.com/Dookietrukie" rel="noreferrer" target="_blank"><GitHub fontSize="medium"/></a>
             </Box>
             
             <Box my={0.5} mx={2}>
-                <Typography variant="h6">Ismael Freitas</Typography>
+                <Typography variant="h6" sx={{fontSize: "1rem"}}>Ismael Freitas</Typography>
                 <a href="https://www.linkedin.com/in/ismael-freitas-6366891b8/" rel="noreferrer" target="_blank"><LinkedIn fontSize="medium"/></a>
                 <a href="https://github.com/IsmaelFreitas98" rel="noreferrer" target="_blank"><GitHub fontSize="medium"/></a>
             </Box>
